ca0600230ccdc9ff613e31cac5d046a17959cc56
Waqar azhar <waqar.azhar@confiz.com>
2015-12-24 19:29:44 +0500

    menu page added

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ var myApp = angular.module('myApp', ['ngRoute','ngAnimate']);
 
 myApp.config(function ($routeProvider) {
 
-    $routeProvider
-        .when('/',{
+    $routeProvider.when('/',{
             controller:'homePageController',
             templateUrl:'templates/homepage/homepage.html'
         }).when('/main-menu',{
             controller:'mainPageController',
             templateUrl:'templates/mainpage/mainpage.html'
+        }).when('/type-menu',{
+            controller:'menuPageController',
+            templateUrl:'templates/menupage/menu.html'
+        }).when('/sub-menu',{
+            controller:'subMenuController',
+            templateUrl:'templates/submenu/submenu.html'
+        }).when('/list-menu',{
+            controller:'listMenuController',
+            templateUrl:'templates/listmenu/listmenu.html'
+        }).when('/timer-page',{
+            controller:'timerPageController',
+            templateUrl:'templates/timerpage/timer.html'
+        }).when('/details-page',{
+            controller:'resultPageController',
+            templateUrl:'templates/resultpage/details.html'
         }).
-        otherwise('/')
+        otherwise('/');
 });
 
