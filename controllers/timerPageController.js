7f5f1b51f9bffb9a132577043fcdd99c336f14ca
Waqar azhar <waqar.azhar@confiz.com>
2015-12-26 16:02:45 +0500

    timer page update

--- controllers/timerPageController.js
+++ controllers/timerPageController.js
@@ -1,9 +1,23 @@
 myApp.controller('timerPageController', ['$scope', function($scope){
 
+	$scope.timeSelected = "";
 	$(".background").mousemove(function( event ) {
   		var w = $(this).width(),
         pct = 360*(+event.pageX)/w,
         bg = "linear-gradient(" + pct + "deg,#4ac1ff,#795bb0)";
         $(".background").css("background-image", bg);
+	});
+
+	
+	$('.clockpicker').clockpicker({
+		placement: 'top',
+		align: 'left',
+		donetext: 'Done'
+	});
+
+	$('.clockpicker').clockpicker()
+	.find('input').change(function(){
+		console.log(this.value);
+		$scope.timeSelected = this.value;
 	});
 }]);
