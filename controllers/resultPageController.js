167ad17557d118183efbcb17a0a928a7b952f508
Waqar azhar <waqar.azhar@confiz.com>
2015-12-26 20:57:51 +0500

    timer page finalized

---
+++ controllers/resultPageController.js
@@ -0,0 +1,10 @@
+myApp.controller('resultPageController', ['$scope', function($scope){
+
+	$(".background").mousemove(function( event ) {
+  		var w = $(this).width(),
+        pct = 360*(+event.pageX)/w,
+        bg = "linear-gradient(" + pct + "deg,#4ac1ff,#795bb0)";
+        $(".background").css("background-image", bg);
+	});
+
+}]);
--- controllers/timerPageController.js
+++ controllers/timerPageController.js
@@ -1,6 +1,5 @@
-myApp.controller('timerPageController', ['$scope', function($scope){
+myApp.controller('timerPageController', ['$scope','$rootScope','$location', function($scope,$rootScope,$location){
 
-	$scope.timeSelected = "";
 	$(".background").mousemove(function( event ) {
   		var w = $(this).width(),
         pct = 360*(+event.pageX)/w,
@@ -8,41 +7,40 @@ myApp.controller('timerPageController', ['$scope', function($scope){
         $(".background").css("background-image", bg);
 	});
 
-	
-	$('.clockpicker').clockpicker({
-		placement: 'top',
-		align: 'left',
-		donetext: 'Done'
-	});
-
-	$('.clockpicker').clockpicker()
-	.find('input').change(function(){
-		console.log(this.value);
-		$scope.timeSelected = this.value;
-		var i = $scope.timeSelected;
-
-var counterBack = setInterval(function(){
-  i--;
-  if(i>0){
-    $('.progress-bar').css('width', i+'%');
-  } else {
-    clearTimeout(counterBack);
-  }
-  
-}, 1000);
-	});
-
-
-
     $(document).ready(function() {
+        
         $('.burning').burn({
-  k: 100,
-  w: 100
-});
+		  k: 100,
+		  w: 100
+		});
+        
         $("h1").lettering();
 
-        // Code goes here
+        $("#slider2").roundSlider({
+		    sliderType: "min-range",
+		    value: 40,
+		    change: function (e) {
 
+		    	var k = 0;
+		 		var i = 100;
+		 		var j = e.value;
+		 		var calc = (i/j);
+
+				var counterBack = setInterval(function(){
+				  if(i>=0){
+				    $('.progress-bar').css('width', k +'%');
+				    k = k + calc;
+				    i = i - calc;
+				  } else {
+				    clearTimeout(counterBack);
+				    $rootScope.$apply(function() {
+			        	$location.path('/details-page');
+			      	});
+				  }
+				  
+				}, 1000);
+		    }
+		});
     });
 
 }]);
