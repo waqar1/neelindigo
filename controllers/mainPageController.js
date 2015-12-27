9fb4d5f5c0be78538f5e0817ee654f31d303bc56
Waqar azhar <waqar.azhar@confiz.com>
2015-12-27 18:22:03 +0500

    main menu animaton changed

--- controllers/mainPageController.js
+++ controllers/mainPageController.js
@@ -11,61 +11,61 @@ myApp.controller('mainPageController', ['$scope', function($scope){
 
 	$( "#Upper" ).hover(
 		function() {
-	        $("#Upper").addClass('animated flip');
+	        $("#Upper").addClass('animated bounce');
 	    },
 	    function() {
-	        $("#Upper").removeClass('animated flip');
+	        $("#Upper").removeClass('animated bounce');
 	    }
 	);
 
 	$('#Upper').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 		$("#Upper").removeClass('rollIn');
 		$("#Upper").removeClass('animated');
-		$("#Upper").removeClass('flip');
+		$("#Upper").removeClass('bounce');
 	});
 
 	$( "#Left" ).hover(
 		function() {
-	        $("#Left").addClass('animated flip');
+	        $("#Left").addClass('animated bounce');
 	    },
 	    function() {
-	        $("#Left").removeClass('animated flip');
+	        $("#Left").removeClass('animated bounce');
 	    }
 	);
 
 	$('#Left').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 		$("#Left").removeClass('rollIn');
 		$("#Left").removeClass('animated');
-		$("#Left").removeClass('flip');
+		$("#Left").removeClass('bounce');
 	});
 
 	$( "#Right" ).hover(
 		function() {
-	        $("#Right").addClass('animated flip');
+	        $("#Right").addClass('animated bounce');
 	    },
 	    function() {
-	        $("#Right").removeClass('animated flip');
+	        $("#Right").removeClass('animated bounce');
 	    }
 	);
 
 	$('#Right').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 		$("#Right").removeClass('rollIn');
 		$("#Right").removeClass('animated');
-		$("#Right").removeClass('flip');
+		$("#Right").removeClass('bounce');
 	});
 
 	$( "#Below" ).hover(
 		function() {
-	        $("#Below").addClass('animated flip');
+	        $("#Below").addClass('animated bounce');
 	    },
 	    function() {
-	        $("#Below").removeClass('animated flip');
+	        $("#Below").removeClass('animated bounce');
 	    }
 	);
 
 	$('#Below').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
 		$("#Below").removeClass('rollIn');
 		$("#Below").removeClass('animated');
-		$("#Below").removeClass('flip');
+		$("#Below").removeClass('bounce');
 	});
 }]);
