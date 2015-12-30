29a65b525f3c2f7f7649a2e2926ff79bb73edb0c
Waqar azhar <waqar.azhar@confiz.com>
2015-12-31 02:12:13 +0500

    overlay changed

--- controllers/mainPageController.js
+++ controllers/mainPageController.js
@@ -6,6 +6,10 @@ myApp.controller('mainPageController', ['$scope', function($scope){
 		  
 		mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
 		mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-        $('.background-menu').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgba(16, 38, 82,0.7), rgba(255, 255, 255,-2.0)), url(assets/images/menubackground.png) no-repeat center center fixed');
+		if((mouseXpercentage > 20 && mouseXpercentage < 80) && (mouseYpercentage > 10 && mouseYpercentage < 80)){
+			$('.background-menu').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgba(16, 38, 82,0.7) 1%, rgba(255, 255, 255,-2.0) 50%), url(assets/images/menubackground.png) no-repeat center center fixed');
+		}else{
+			$('.background-menu').css('background', 'radial-gradient(at 50% 50%, rgba(16, 38, 82,0.7) 2%, rgba(255, 255, 255,-2.0) 50%), url(assets/images/menubackground.png) no-repeat center center fixed');
+		}
 	});
 }]);
